8f0f3837fbae573e244233f4b58044f4e18a073e
Bersikan <khozeyev@gmail.com>
2024-03-20 20:39:54 +0200

    add some

---
+++ src/main/java/configurations/BaseConfig.java
@@ -0,0 +1,16 @@
+package configurations;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeSuite;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseConfig {
+    @BeforeSuite
+    public static void init() {
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "";
+        Configuration.browserSize = "1280x1024";
+        open("https://github.com/");
+    }
+}
