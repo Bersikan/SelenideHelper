72bcca1482a48c305fe19fe8bdf846829a66d6e8
Bersikan <khozeyev@gmail.com>
2024-06-15 00:09:28 +0300

    some rework

--- src/main/groovy/services/reqres/CommonBaseSpecification.groovy
+++ src/main/groovy/services/reqres/CommonBaseSpecification.groovy
@@ -3,6 +3,7 @@ package services.reqres
 import general_helpers.ThreadSafeBaseURI
 import io.qameta.allure.restassured.AllureRestAssured
 import io.restassured.RestAssured
+import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
 
 import static io.restassured.RestAssured.baseURI
@@ -13,5 +14,11 @@ class CommonBaseSpecification {
         RestAssured.filters([new AllureRestAssured()])
         ThreadSafeBaseURI.setEndPoint("https://reqres.in")
         baseURI = ThreadSafeBaseURI.getEndPoint()
+        println("Tread started: "+ Thread.currentThread().getId())
+    }
+
+    @AfterMethod(alwaysRun = true)
+    void down() {
+        println("Tread closed: "+ Thread.currentThread().getId())
     }
 }
